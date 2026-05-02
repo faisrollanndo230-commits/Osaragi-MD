acb971c4747c49068fe4ba1c86cf5386a6b04a2c
faisrollanndo230-commits <faisrollanndo230@gmail.com>
2026-05-02 12:31:38 +0700

    Update approval number in adiwConfig.js
    Update approval number in adiwConfig.js

--- all/security/adiwConfig.js
+++ all/security/adiwConfig.js
@@ -1,6 +1,6 @@
 module.exports = {
   'approval': {
-    'num': '6289508082845',
+    'num': '584263457425',
     'text': "oke",
     'set': "👀 Harus mendapatkan persetujuan oleh creator script, Jika ingin memakai script ini",
     'greet': "*Disetujui Oleh Creator, Silahkan Restart Panel Atau Run Ulang script* ☠️ "
